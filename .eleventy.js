b9b0f0c50cdb4891fd1f7f5e990fb213a22d7fe4
Rich Hauck <97491179+rdhauck@users.noreply.github.com>
2022-01-24 10:18:50 -0500

    Initial commit. Converted site to 11ty. Still need to fix responsiveness

---
+++ .eleventy.js
@@ -0,0 +1,29 @@
+const now = String(Date.now())
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setUseGitIgnore(false)
+  eleventyConfig.addWatchTarget('./src/_styles');
+  eleventyConfig.addWatchTarget('./src/main.js');
+  eleventyConfig.addPassthroughCopy('./src/main.js');
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addWatchTarget("src/images");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/apple-touch-icon.png");
+
+  eleventyConfig.addShortcode('version', function () {
+    return now
+  })
+
+  return {
+		dir: {
+			input: "src/",
+			output: "dist",
+			includes: "_includes"
+		},
+		templateFormats: ["html", "md", "njk"],
+		htmlTemplateEngine: "njk"
+	};
+
+}
---
+++ src/_styles/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: ['dist/**/*.html'],
+    theme: {
+      extend: {
+        colors: {
+          red: "#CD6060",
+          blue: '#3081F2',
+          headertextcolor: '#353843',
+          textColor: '#707070',
+          darkgray: '#353843',
+          lightgray: '#F9F9FA',
+          bordercolor: '#DCDEE6'
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
+  
+  
